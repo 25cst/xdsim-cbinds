72c0dc335db04a10d98807976a2e78a32000d9aa
Siriusmart <sirius@siri.ws>
2026-01-26 20:35:35 +0000

    made fields public

--- src/v0/component/conn_path.rs
+++ src/v0/component/conn_path.rs
@@ -3,16 +3,16 @@ use crate::common::Slice;
 #[repr(C)]
 pub struct ConnSegment {
     /// [ Vec2 ]
-    path: Slice,
+    pub path: Slice,
     /// if is NONE, then write NULL
-    next: *const ConnJunction,
+    pub next: *const ConnJunction,
 }
 
 /// if is NONE, then write NULL
 #[repr(C)]
 pub struct ConnJunction {
-    up: *const ConnSegment,
-    right: *const ConnSegment,
-    down: *const ConnSegment,
-    left: *const ConnSegment,
+    pub up: *const ConnSegment,
+    pub right: *const ConnSegment,
+    pub down: *const ConnSegment,
+    pub left: *const ConnSegment,
 }
